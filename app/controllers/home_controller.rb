24d5c2eb4027ee6123ebf790725cd6b23158dba6
Hex <thehexreader@gmail.com>
2022-08-15 19:18:58 +0300

    fixing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@
 
 # Home controller
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @current_user_email = if current_user.nil?
+                            'No user logged in'
+                          else
+                            current_user['email']
+                          end
+  end
 end

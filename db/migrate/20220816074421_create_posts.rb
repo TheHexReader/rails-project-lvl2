29fe097e4eef2556b91fa5af3e2dc8570dee7072
Hex <thehexreader@gmail.com>
2022-09-07 14:49:53 +0300

    fixing

--- db/migrate/20220816074421_create_posts.rb
+++ db/migrate/20220816074421_create_posts.rb
@@ -7,8 +7,8 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.string :title
       t.text :body
       t.string :creator
-      t.integer :category_id
       t.string :category
+      t.integer :category_id
 
       t.timestamps
     end

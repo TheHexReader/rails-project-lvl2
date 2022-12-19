7fddbb0965039ed315ccd15922a0027259152c43
Hex <thehexreader@gmail.com>
2022-12-19 12:26:58 +0300

    Validate category_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params.merge(creator: current_user['id']))
 
-    if @post.save && !post_params[:category_id].nil?
+    if @post.save && !post_params[:category_id].nil? && check_if_category_is_chosen
       redirect_to post_path(@post)
     else
       render :new
@@ -41,10 +41,15 @@ class PostsController < ApplicationController
   def update
     @post = Post.find params[:id]
 
+    unless check_if_category_is_chosen
+      redirect_to edit_post_path(@post)
+      return
+    end
+
     if @post.update(post_params.merge(creator: current_user['id']))
       redirect_to @post, notice: t('success')
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
@@ -62,5 +67,12 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body, :creator, :category_id)
+  end
+
+  def check_if_category_is_chosen
+    return true unless post_params[:category_id] == ''
+
+    flash[:notice] = 'Не выбранна категория'
+    false
   end
 end

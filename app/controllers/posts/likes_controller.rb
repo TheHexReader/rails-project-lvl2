14b31393722207dfce60faa9b6605717af2cf370
Hex <thehexreader@gmail.com>
2023-01-11 10:19:20 +0300

    test

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -9,17 +9,15 @@ module Posts
     def create
       p '--- Like params ---'
       p like_params
-      p '--- Processed params ---'
-      p processed_params
       
       return unless PostLike.where({ id: like_params[:id] }).empty?
 
-      @like = PostLike.new(processed_params)
+      @like = PostLike.new(like_params)
 
       if @like.save
-        redirect_to post_path(processed_params[:post]), notice: t('success')
+        redirect_to post_path(like_params[:post]), notice: t('success')
       else
-        redirect_to post_path(processed_params[:post]), status: :unprocessable_entity
+        redirect_to post_path(like_params[:post]), status: :unprocessable_entity
       end
     end
 
@@ -27,26 +25,22 @@ module Posts
       @like = PostLike.find(like_params[:id])
 
       if @like.delete
-        redirect_to post_path(processed_params[:post]), notice: t('success')
+        redirect_to post_path(like_params[:post]), notice: t('success')
       else
-        redirect_to post_path(processed_params[:post]), status: :unprocessable_entity
+        redirect_to post_path(like_params[:post]), status: :unprocessable_entity
       end
     end
 
     protected
 
-    def processed_params
-      p '--- Test find_by start ---'
-      out = {
-        user: current_user,
-        post: Post.find_by(id: like_params[:post_id])
-      }
-      p '--- Test find_by end ---'
-      out
-    end
-
     def like_params
-      params.permit(:post_id, :id)
+      permited = params.permit(:post_id, :id)
+
+      {
+        user: current_user,
+        post: Post.find_by(id: permited[:post_id]),
+        id: permited[:id]
+      }
     end
   end
 end

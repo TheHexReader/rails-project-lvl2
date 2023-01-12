d09bbf12e8ef479eb34dca48503ff6275d7752db
Hex <thehexreader@gmail.com>
2023-01-12 15:05:45 +0300

    test

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :posts
+  has_many :posts, class_name: 'Post', foreign_key: 'category_id'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'PostComment'
   has_many :likes, class_name: 'PostLike'
 
-  has_one :creator_id, class_name: 'User'
-  has_one :category_id, class_name: 'Category'
+  has_one :category
+  has_one :creator, class_name: 'User'
 
   validates :title, presence: true
   validates :body, presence: true

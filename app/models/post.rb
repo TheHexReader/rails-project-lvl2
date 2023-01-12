bf8e9f4ae3520b97dc334d094382dfe6bef65d1f
Hex <thehexreader@gmail.com>
2023-01-12 15:43:28 +0300

    test

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'PostComment'
   has_many :likes, class_name: 'PostLike'
 
-  has_one :category
-  has_one :creator, class_name: 'User'
+  has_one :user, foreign_key: 'creator_id'
+  has_one :category, foreign_key: 'category_id'
 
   validates :title, presence: true
   validates :body, presence: true

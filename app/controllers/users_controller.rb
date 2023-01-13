2149c7283274a2b16f33babeb434511945aa981c
Hex <thehexreader@gmail.com>
2023-01-13 17:20:35 +0300

    fixed own test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find_by(id: params[:id])
-    @posts = @user.posts
+    @posts = Post.where(creator_id: @user)
   end
 end
